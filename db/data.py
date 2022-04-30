2b721401c2f7a9abc5fdf7cf759fdd50bba7a998
woohan100 <80763549+woohan100@users.noreply.github.com>
2022-04-30 10:12:41 +0900

    New modules: db.data, db.query db.connector  - db_executemany: new function  - func_name: new local variables

---
+++ db/data.py
@@ -0,0 +1,70 @@
+import mariadb
+
+from db.connector import db_execute, db_executemany
+from db.query import list_to_select, dict_to_where
+
+
+def select_one_row(conn: mariadb.connection, db_table: str, unique_keys: dict, select_columns: list, return_col_descs=False):
+    func_name = 'select_one_row'
+
+    if not select_columns:
+        select_str = '*'
+    else:
+        select_str = list_to_select(select_columns)
+
+    where_str = dict_to_where(unique_keys)
+
+    query = f"SELECT {select_str} FROM {db_table} WHERE {where_str}"
+    cur = conn.cursor()
+    db_execute(cur, query)
+    rows = cur.fetchall()
+    if not rows:
+        if not return_col_descs:
+            return None
+        return None, None
+    elif len(rows) != 1:
+        print(f">>> {func_name} -  DB Data Error: {unique_keys} is not unique in '{db_table}'.")
+        if not return_col_descs:
+            return None
+        return None, None
+
+    if not return_col_descs:
+        return rows[0]
+
+    col_descs = cur.description
+
+    return rows[0], col_descs
+
+
+def select_one_row_pack_into_dict(conn: mariadb.connection, db_table: str, unique_keys: dict, select_columns: list):
+    (row, col_descs) = select_one_row(conn, db_table, unique_keys, select_columns, return_col_descs=True)
+
+    col_names = [col[0] for col in col_descs]
+    row_dict = dict(zip(col_names, row))
+
+    return row_dict
+
+
+def insert_dict(conn: mariadb.connection, db_table: str, new_data: dict):
+    select_str = list_to_select(list(new_data.keys()))
+    placeholder_str = ", ".join(['?'] * len(new_data))
+    insert_data = tuple(new_data.values())
+
+    query = f"INSERT INTO {db_table} ({select_str}) VALUES ({placeholder_str})"
+    cur = conn.cursor()
+    insert_success = db_execute(cur, query, python_data=insert_data)
+
+    return insert_success
+
+
+def insert_many(conn: mariadb.connection, db_table: str, columns: list, many_values: list):
+    # many_values is a list of values list
+    select_str = list_to_select(columns)
+    placeholder_str = ", ".join(['?'] * len(select_str))
+    insert_data = [tuple(values) for values in many_values]
+
+    query = f"INSERT INTO {db_table} ({select_str}) VALUES ({placeholder_str})"
+    cur = conn.cursor()
+    insert_success = db_executemany(cur, query, insert_data)
+
+    return insert_success
