252ae65495bc2875375840ab5d9272f217b155c8
woohan100 <80763549+woohan100@users.noreply.github.com>
2022-05-07 12:05:57 +0900

    Renamed Modules  - query: functions for query execution  - query_str: functions for building a query string

--- db/query.py
+++ db/query.py
@@ -1,77 +1,74 @@
-import re
-from datetime import date, datetime
+import mariadb
+
+from db.connector import db_execute, db_executemany
+from db.query_str import list_to_select, dict_to_where
 
 
-def list_to_select(column_list: list):
-    if not column_list:
-        return None
+def select_one_row(conn: mariadb.connection, db_table: str, unique_keys: dict, select_columns: list, return_col_descs=False):
+    func_name = 'select_one_row'
 
-    separator = ', '
-    select_str = separator.join(column_list)
-
-    return select_str
-
-
-def dict_to_set(column_dict: dict, python_data_no=0):
-    func_name = 'dict_to_set'
-
-    if not column_dict:
-        return None
-
-    if python_data_no > len(column_dict):
-        print(f">>> {func_name} -  Warning: No. of python_data ({python_data_no}) is greater than no. of set columns ({len(column_dict)}).")
-
-    set_str = ''
-    for i, (key, value) in enumerate(column_dict.items()):
-        if set_str:
-            set_str += ', '
-
-        if (i + 1) <= python_data_no:
-            set_str += f"{key}=?"
-        else:
-            set_str += f"{key}={python_data_to_sql_value(value)}"
-
-    return set_str
-
-
-def dict_to_where(column_dict: dict):
-    where_str = ''
-    for key, value in column_dict.items():
-        if where_str:
-            where_str += ' and '
-        where_str += f"{key}={python_data_to_sql_value(value)}"
-
-    return where_str
-
-
-def list_to_values(python_values: list):
-    sql_values = list(map(python_data_to_sql_value, python_values))
-    separator = ', '
-    values_str = separator.join(sql_values)
-
-    return values_str
-
-
-def python_data_to_sql_value(python_data):
-    func_name = 'python_data_to_sql_value'
-    sql_function_pattern = r'\$[A-Z0-9_]+\(.*\)'
-
-    if python_data is None:
-        sql_value = "NULL"
-    elif isinstance(python_data, datetime):
-        sql_value = f"'{python_data.strftime('%Y-%m-%d %H:%M:%S')}'"
-    elif isinstance(python_data, date):
-        sql_value = f"'{python_data.strftime('%Y-%m-%d')}'"
-    elif isinstance(python_data, int) or isinstance(python_data, float):
-        sql_value = f"{python_data}"
-    elif isinstance(python_data, str):
-        if re.fullmatch(sql_function_pattern, python_data):
-            # SQL function in capital letters
-            sql_value = f"{python_data[1:]}"
-        else:
-            sql_value = f"'{python_data}'"
+    if not select_columns:
+        select_str = '*'
     else:
-        sql_value = None
-        print(f">>> {func_name} -  Unknown data type:{type(python_data)}, {python_data}.")
+        select_str = list_to_select(select_columns)
 
-    return sql_value
+    where_str = dict_to_where(unique_keys)
+
+    query = f"SELECT {select_str} FROM {db_table} WHERE {where_str}"
+    cur = conn.cursor()
+    db_execute(cur, query)
+    rows = cur.fetchall()
+    if not rows:
+        if not return_col_descs:
+            return None
+        return None, None
+    elif len(rows) != 1:
+        print(f">>> {func_name} -  DB Data Error: {unique_keys} is not unique in '{db_table}'.")
+        if not return_col_descs:
+            return None
+        return None, None
+
+    if not return_col_descs:
+        return rows[0]
+
+    col_descs = cur.description
+
+    return rows[0], col_descs
+
+
+def select_one_row_pack_into_dict(conn: mariadb.connection, db_table: str, unique_keys: dict, select_columns: list):
+    (row, col_descs) = select_one_row(conn, db_table, unique_keys, select_columns, return_col_descs=True)
+
+    col_names = [col[0] for col in col_descs]
+    row_dict = dict(zip(col_names, row))
+
+    return row_dict
+
+
+def insert_dict(conn: mariadb.connection, db_table: str, new_data: dict):
+    select_str = list_to_select(list(new_data.keys()))
+    placeholder_str = ", ".join(['?'] * len(new_data))
+    insert_data = tuple(new_data.values())
+
+    query = f"INSERT INTO {db_table} ({select_str}) VALUES ({placeholder_str})"
+    cur = conn.cursor()
+    insert_success = db_execute(cur, query, python_data=insert_data)
+
+    return insert_success
+
+
+def insert_many(conn: mariadb.connection, db_table: str, columns: list, many_values: list):
+    # many_values is a list of values list
+    select_str = list_to_select(columns)
+
+    #placeholder_str = ", ".join(['?'] * len(select_str))
+    placeholder_str = ", ".join(['?'] * len(columns))
+
+    # insert_data = [tuple(values) for values in many_values]
+    insert_data = [tuple(d[k] for k in columns) for d in many_values]
+
+    query = f"INSERT INTO {db_table} ({select_str}) VALUES ({placeholder_str})"
+    cur = conn.cursor()
+    insert_success = db_executemany(cur, query, insert_data)
+
+    return insert_success
